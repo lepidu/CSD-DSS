4087df2d89b52c062d5a5ad5441bd38a6942ed22
XIAOHUI-WENG2020 <73845398+XIAOHUI-WENG2020@users.noreply.github.com>
2023-05-07 21:41:39 +0100

    a login page with a database

--- CSD-DSS-main/crud-test-1/src/main/java/com/example/crud/controller/FlightsController.java
+++ CSD-DSS-main/crud-test-1/src/main/java/com/example/crud/controller/FlightsController.java
@@ -1,7 +1,5 @@
 package com.example.crud.controller;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +16,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.crud.model.Flights;
 import com.example.crud.repository.FlightsRepository;
+import com.example.crud.dto.UserDto;
+import com.example.crud.entity.User;
+import com.example.crud.service.UserService;
 
-
-
+import jakarta.validation.Valid;
 
 
 @Controller
@@ -30,12 +31,63 @@ public class FlightsController {
 	@Autowired
 	 private FlightsRepository repository;
 	
+	
 	public FlightsController(FlightsRepository repository) {
 		
 		this.repository = repository;
 	}
+	
 
-	@GetMapping("/index")
+    private UserService userService;
+
+    public FlightsController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("index")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    // handler method to handle user registration request
+    @GetMapping("register")
+    public String showRegistrationForm(Model model){
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    // handler method to handle register user form submit request
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto user,
+                               BindingResult result,
+                               Model model){
+        User existing = userService.findByEmail(user.getEmail());
+        if (existing != null) {
+            result.rejectValue("email", null, "There is already an account registered with that email");
+        }
+        if (result.hasErrors()) {
+            model.addAttribute("user", user);
+            return "register";
+        }
+        userService.saveUser(user);
+        return "redirect:/register?success";
+    }
+
+    @GetMapping("/users")
+    public String listRegisteredUsers(Model model){
+        List<UserDto> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "users";
+    }
+	
+
+	@GetMapping("/home")
 	public String home(Model model, @RequestParam(defaultValue = "0") int page) {
 	    int pageSize = 10; // The number of items per page
 	    
@@ -47,7 +99,7 @@ public class FlightsController {
 	    model.addAttribute("currentPage", page);
 	    model.addAttribute("totalPages", flights.getTotalPages());
 	    
-	    return "index";
+	    return "home";
 	}
 
 
---
+++ CSD-DSS-main/crud-test-1/src/main/java/com/example/crud/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.crud.repository;
+
+import com.example.crud.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
