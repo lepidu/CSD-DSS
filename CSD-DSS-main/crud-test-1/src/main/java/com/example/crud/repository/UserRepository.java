b7af7834e179a17b028242413fe23b934a0f4625
XIAOHUI-WENG2020 <73845398+XIAOHUI-WENG2020@users.noreply.github.com>
2023-05-08 12:16:53 +0100

    del

--- CSD-DSS-main/crud-test-1/src/main/java/com/example/crud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud.repository;
-
-import com.example.crud.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface UserRepository extends MongoRepository<User, Long> {
-    User findByEmail(String email);
-}
